8394ab1255a6cb9e0b57feed1a2b48afda6c3c2a
morpheus9631 <morpheus.hsieh@gmail.com>
2020-08-19 14:14:44 +0800

    commit

---
+++ configs/config_train.py
@@ -0,0 +1,19 @@
+# config.py
+from yacs.config import CfgNode as CN
+
+_C = CN()
+
+_C.WORK = CN()
+_C.WORK.ROOT_PATH = "D:\\GitWork\\ants_and_bees\\"
+
+_C.DATA = CN()
+_C.DATA.ROOT_PATH = "D:\\Dataset\\hymenoptera_data\\"
+_C.DATA.TRAIN_DIR = "train"
+_C.DATA.VALIDATE_DIR = "val"
+
+
+def get_cfg_defaults():
+    """Get a yacs CfgNode object with default values for the project."""
+    # Return a clone so that the defaults will not be altered
+    # This is for the "local variable" use pattern
+    return _C.clone()
