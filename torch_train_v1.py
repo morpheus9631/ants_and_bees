93ad1d8585bb86804a6a4891bb435d23807c7b06
morpheus9631 <morpheus.hsieh@gmail.com>
2020-08-31 23:17:27 +0800

    commit

--- torch_train_v1.py
+++ torch_train_v1.py
@@ -36,10 +36,10 @@ def train_model(dataloaders, device, dataset_sizes,
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        print('-' * 30)
 
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
+        for phase in ['train', 'valid']:
             if phase == 'train':
                 model.train()  # Set model to training mode
             else:
@@ -81,7 +81,7 @@ def train_model(dataloaders, device, dataset_sizes,
                 phase, epoch_loss, epoch_acc))
 
             # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == 'valid' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
@@ -106,55 +106,64 @@ def main():
     cfg.freeze()
     print('\n', cfg)
 
-    trans_mean = [0.485, 0.456, 0.406]
-    trans_std  = [0.229, 0.224, 0.225]
-
     normalize = transforms.Normalize(
-        mean = trans_mean,
-        std  = trans_std
+        mean = [0.485, 0.456, 0.406],
+        std  = [0.229, 0.224, 0.225]
     )
 
     # Transforms
-    train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        normalize
-    ])
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+        ]),
+        'valid': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize
+        ])
+    }
 
-    valid_transform = transforms.Compose([
-        transforms.ToTensor(),
-        normalize
-    ])
+    ClassNames = list(data_transforms.keys())
+    print('\nClass names: ', ClassNames)
 
     # Datasets
-    proc_path = cfg.DATA.PROCESSED_PATH
-    trainSet = datasets.ImageFolder(
-        os.path.join(proc_path, 'train'), train_transform
-    )
+    ProcPath = cfg.DATA.PROCESSED_PATH
 
-    validSet = datasets.ImageFolder(
-        os.path.join(proc_path, 'valid'), valid_transform
-    )
+    image_datasets = {
+        x: datasets.ImageFolder(os.path.join(ProcPath, x), data_transforms[x])
+        for x in ClassNames
+    }
+
+    trainSet = image_datasets['train']
+
+    class_indexes = trainSet.class_to_idx
+    print('class to index: ', class_indexes)
+
+    trainX, trainY = trainSet[0]
+    print('\nimage:')
+    print(trainX)
+    print('label:', trainY)    
 
     BatchSize = cfg.TRAIN.BATCH_SIZE
     NumWorkers = cfg.TRAIN.NUM_WORKERS
     
-    trainLoader = DataLoader(
-        trainSet, batch_size=BatchSize, shuffle=True, num_workers=NumWorkers
-    )
-
-    validLoader = DataLoader(
-        trainSet, batch_size=BatchSize, shuffle=False, num_workers=NumWorkers
-    )
-
-    dataset_sizes = {
-        'train': len(trainSet), 'valid': len(validSet)
+    dataloaders = {
+        'train': DataLoader(image_datasets['train'],
+            batch_size=BatchSize, shuffle=True,  num_workers=NumWorkers
+        ),
+        'valid': DataLoader(image_datasets['valid'],
+            batch_size=BatchSize, shuffle=False, num_workers=NumWorkers
+        )
     }
 
-    class_names = trainSet.classes
-    class_to_idx = trainSet.class_to_idx
-    print('\nClass to idx: ', class_to_idx)
+    dataset_sizes = {
+        x: len(image_datasets[x]) for x in ClassNames
+    }
+    print('dataset sizes: ', dataset_sizes)
 
     # Use GPU
     isUseGpu = torch.cuda.is_available()
@@ -162,29 +171,33 @@ def main():
 
     # Model summary
     model_ft = models.resnet18(pretrained=True)
-    print(); print(model_ft)
+    # print(); print(model_ft)
 
-    # num_ftrs = model_ft.fc.in_features
+    num_ftrs = model_ft.fc.in_features
     
-    # # Here the size of each output sample is set to 2.
-    # # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-    # model_ft.fc = nn.Linear(num_ftrs, 2)
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+    model_ft.fc = nn.Linear(num_ftrs, 2)
 
-    # model_ft = model_ft.to(device)
+    model_ft = model_ft.to(device)
 
-    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
-    # # Observe that all parameters are being optimized
-    # optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # Observe that all parameters are being optimized
+    lr = cfg.TRAIN.LEARNING_RATE
+    momentum = cfg.TRAIN.MOMENTUM
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=lr, momentum=momentum)
 
-    # # Decay LR by a factor of 0.1 every 7 epochs
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-
-    # model_ft = train_model(
-    #     dataloaders, device, dataset_sizes,
-    #     model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25,
-    # )
+    # Train and evaluate
+    print()
+    num_epochs = cfg.TRAIN.NUM_EPOCHS
+    model_ft = train_model(
+        dataloaders, device, dataset_sizes,
+        model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=num_epochs,
+    )
     return(0)
 
 
